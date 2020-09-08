2149c69117f1cbbf9756cc86df6f1458d4acaf53
ViLSa <vishnuagbly@gmail.com>
2020-09-08 19:48:59 +0530

    updated faqs

--- lib/data/faqs.dart
+++ lib/data/faqs.dart
@@ -1,12 +1,40 @@
 Map<String, List<Map<String, String>>> allQuestions = {
   "questions": [
     {
-      'question': 'Why do I have this? I never downloaded this app?',
-      'answer': 'This is because we forcefully downloaded this app on your phone. Either by means of life threat, ragging, bullying, black mailing or brain washing.\nNote:- If any of these options does not explain your situation than note that we are in no way responsible for this app on your phone.\nPeople who usually downloads our app from their own free will are usually either mad, depressed or brain washed by any third-party.',
+      "question": "I just bought a device. How to connect it?",
+      "answer":
+          "1. Reset the device by turning the device on and off thrice.\n\n2. Yellow LED on the device will start to blink.\n\n3. Go to connect device option in the app by clicking on the + icon in the right hand top corner.\n\n4. Find your device’s default name which came with the device.\n\n5. Select your Wifi network and enter Wifi Password.\n\n6. Yellow light will become stable since the connection was successful."
     },
     {
-      'question': 'Why the fuck I cannot move map?',
-      'answer': "This is because it is a fucking picture not an actual map.\nSorry for inconvenience but currently we are unable to show maps so for now we are only presenting oh how would the map look like",
+      "question": "My device is not working properly. How to reset it?",
+      "answer":
+          "1. Reset the device by turning the device on and off thrice.\n\n2. Yellow LED on the device will start to blink.\n\n3. Go to connect device option in the app by clicking on the + icon in the right hand top corner.\n\n4. Find your device’s default name which came with the device.\n\n5. Select your Wifi network and enter Wifi Password.\n\n6. Yellow light will become stable since the connection was successful."
     },
+    {
+      "question":
+          "What should I do when my device fails to connect to the network?",
+      "answer":
+          "First ensure that you have a stable internet connection and the device is in the range of the Wi-Fi.\n\nIf you are using a 5 GHz network, please note that currently the 5 GHz Wi-Fi networks are not supportable. Kindly, change to 2.4 GHz Wi-Fi network through your router settings.\n\nIf you are still facing any problems, contact our customer care at 1800-4575-337."
+    },
+    {
+      "question": "How do I share a device?",
+      "answer":
+          "To share a device you can add a device to multiple accounts, but this might cause problems due to different device configurations. The best and easiest way to share a device is to log in using a same account."
+    },
+    {
+      "question": "What should I do when a firmware upgrade fails?",
+      "answer":
+          "Firmware upgrades fail when a device is powered off or loses internet connection during the firmware upgrade. Ensure that the device is receiving continuous electricity and internet and retry firmware upgrade.\n\nIf you are still facing any problems, contact our customer care at 1800-4575-337."
+    },
+    {
+      "question":
+          "My device can't detect 5 GHz Wi-Fi network. What should I do?",
+      "answer":
+          "Currently, the 5 GHz Wi-Fi networks are not supportable. Kindly, change to 2.4 GHz Wi-Fi network through your router settings."
+    }
+    //{
+//'question': 'Why do I have this app? I never downloaded this app?',
+//'answer': 'This is because we forcefully downloaded this app on your phone. Either by means of life threat, ragging, bullying, black mailing or brain washing.\nNote:- If any of these options does not explain your situation than note that we are in no way responsible for this app on your phone.\nPeople who usually downloads our app from their own free will are usually either mad, depressed or brain washed by any third-party.',
+//},
   ]
 };
